e794896211d8f3c36209ee0344188ac2c5bcaf6c
Ranjitha9945 <103716475+Ranjitha9945@users.noreply.github.com>
2022-05-12 10:13:50 +0530

    Update CampainsTest.java

--- src/test/java/crm/crm/campaignsTest/CampainsTest.java
+++ src/test/java/crm/crm/campaignsTest/CampainsTest.java
@@ -14,6 +14,7 @@ public class CampainsTest
 	public void modifyCampaign()
 	{
 		System.out.println("execute modifyCampaign");
+		System.out.println("execute modifyCampaign2");
 	}
 
 	@Test(groups="regressionTest")
